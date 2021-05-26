6b58f7a226fcf1b3f3f184c6d718526ed299210a
wallace moreira da silva <wallaceinfofuturo@gmail.com>
2021-05-26 09:43:25 -0300

    Ajuste no controller

--- AspNetCore_WebApi_Serilog/AspNetCore_WebApi_Serilog/Controllers/WeatherForecastController.cs
+++ AspNetCore_WebApi_Serilog/AspNetCore_WebApi_Serilog/Controllers/WeatherForecastController.cs
@@ -20,19 +20,39 @@ namespace AspNetCore_WebApi_Serilog.Controllers
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
-        }
+			logger.LogInformation("WeatherForecast instanciando ILogger");
+		}
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-    }
+		[HttpGet]
+		public IEnumerable<WeatherForecast> Get()
+		{
+			_logger.LogInformation("endpoint Get => WeatherForecast.Get() ");
+			int count;
+
+			try
+			{
+				for (count = 0; count <= 5; count++)
+				{
+					if (count == 3)
+						throw new Exception("Ocorreu uma Exception Aleatória...");
+					else
+						_logger.LogInformation("Número de iterações {count}");
+				}
+
+				var rng = new Random();
+
+				return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+				{
+					Date = DateTime.Now.AddDays(index),
+					TemperatureC = rng.Next(-20, 55),
+					Summary = Summaries[rng.Next(Summaries.Length)]
+				})
+				.ToArray();
+			}
+			catch (Exception ex)
+			{
+				throw;
+			}
+		}
+	}
 }
--- AspNetCore_WebApi_Serilog/AspNetCore_WebApi_Serilog/Startup.cs
+++ AspNetCore_WebApi_Serilog/AspNetCore_WebApi_Serilog/Startup.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AspNetCore_WebApi_Serilog
 {
@@ -21,8 +15,7 @@ namespace AspNetCore_WebApi_Serilog
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -32,8 +25,7 @@ namespace AspNetCore_WebApi_Serilog
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AspNetCore_WebApi_Serilog", Version = "v1" });
             });
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+       
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
