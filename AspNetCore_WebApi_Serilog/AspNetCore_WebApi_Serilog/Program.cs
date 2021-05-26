b1a7ab47d38e71f6284b6a4abb16b31f2e879e06
wallace moreira da silva <wallaceinfofuturo@gmail.com>
2021-05-26 08:40:41 -0300

    Ajustando a class Program

--- AspNetCore_WebApi_Serilog/AspNetCore_WebApi_Serilog/Program.cs
+++ AspNetCore_WebApi_Serilog/AspNetCore_WebApi_Serilog/Program.cs
@@ -1,26 +1,44 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AspNetCore_WebApi_Serilog
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			//Lê as definições do arquivo appsettings.json
+			var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+			//Usamos uma instancia de LoggerConfiguration e a partir das informações obtidas no
+			//arquivo appsettings criamos um logger usando os sinks, 
+			//os enrichers e as demais definições
+			Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(config).CreateLogger();
+
+			try
+			{
+				Log.Information("API Inicializando...");
+				CreateHostBuilder(args).Build().Run();
+			}
+			catch (Exception ex)
+			{
+				Log.Fatal(ex, "A aplicação falhou ao iniciar.");
+			}
+			finally
+			{
+				Log.CloseAndFlush();
+			}
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.UseSerilog() //define o Serilog como provedor de Log
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+	}
 }
